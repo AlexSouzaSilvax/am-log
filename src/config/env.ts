dcaf15eb917a1a934822652d6e45f13411cbac68
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2026-03-08 06:10:39 -0300

    fix: config project  to vercel

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,15 @@
+function getEnv(name: string): string {
+  const value = import.meta.env[name];
+
+  if (!value) {
+    throw new Error(`❌ Missing environment variable: ${name}`);
+  }
+
+  return value;
+}
+
 export const env = {
-  supabaseUrl: import.meta.env.VITE_SUPABASE_URL!,
-  supabaseKey: import.meta.env.VITE_SUPABASE_ANON_KEY!,
-  googleMapsKey: import.meta.env.VITE_GOOGLE_MAPS_KEY!,
+  supabaseUrl: getEnv("VITE_SUPABASE_URL"),
+  supabaseKey: getEnv("VITE_SUPABASE_ANON_KEY"),
+  googleMapsKey: getEnv("VITE_GOOGLE_MAPS_KEY"),
 };
